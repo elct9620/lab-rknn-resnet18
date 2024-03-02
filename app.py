f7680a0b572acb73c5452e990dc9b8501a765b3b
Aotokitsuruya <contact@aotoki.me>
2024-03-02 21:38:52 +0800

    Add device info for testing

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from rknnlite.api import RKNNLite
+
+DEVICE_COMPATIBLE_NODE = '/proc/device-tree/compatible'
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    device_compatible_str = ""
+    with open(DEVICE_COMPATIBLE_NODE) as f:
+        device_compatible_str = f.read()
+
+    return {"device_compatible": device_compatible_str}
