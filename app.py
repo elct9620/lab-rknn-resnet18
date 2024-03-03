515b67ad38fa88ba75779d7e77c0267db8a32d0b
Aotokitsuruya <contact@aotoki.me>
2024-03-03 16:22:43 +0800

    Implement inference api

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import re
+import numpy as np
+import cv2
 from typing import Annotated
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, UploadFile
 from rknnlite.api import RKNNLite
 
 MODEL_PATH = 'yolov5s-640-640.rknn'
@@ -26,3 +28,15 @@ async def root(model: Annotated[RKNNLite, Depends(model)]):
     driver_version = re.search(r'DRV: (\d+\.\d+\.\d+)', sdk_version).group(1)
 
     return {"api_version": api_version, "driver_version": driver_version}
+
+@app.post("/inference")
+async def inference(model: Annotated[RKNNLite, Depends(model)], image: UploadFile):
+    image = await image.read()
+    image = np.frombuffer(image, dtype=np.uint8)
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    image = cv2.resize(image, (640, 640))
+    image = np.expand_dims(image, axis=0)
+
+    outputs = model.inference(inputs=[image])
+
+    return {"inference": outputs[0]}
